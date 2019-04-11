e4cb4224b20df7ccf1f63449054caa8c3d1c3664
GaddamMeghana578 <maggi.reddy84@gmail.com>
2019-04-11 14:12:19 -0400

    Added a coverpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import CoverPage from "./views/coverpage";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>This is a MERN stack project</h1>
+        <CoverPage />
       </div>
     );
   }
---
+++ src/views/coverpage.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export default class coverpage extends Component {
+  render() {
+    return (
+        <div className = "container">
+        <div className="row">
+            <div className="col-xs-push-3 col-sm-9">
+                <p></p>
+            </div>
+            <div className = "form-group col-md-push-9 col-md-10">
+                <audio controls>
+                    <source src="/audio/Instructions.m4a" type = "audio/mp4"/>
+                </audio>
+            </div>
+            <div className="col-sm-12">
+                <h3 className="alert alert-warning"><strong>Visitor Portal</strong></h3>
+            </div>
+            <div className="form-group col-md-push-10 col-md-12">
+                <button className="btn btn-warning" data-dismiss="modal" data-toggle="modal" data-target="#mymodal" data-placement="left" title="If you are a visitor kindly enroll">Enroll</button>&nbsp;
+                <button className="btn btn-default" data-toggle="tooltip" data-placement="right" title="If you are an administrator kindly login"><a href="/#!/Login">Login</a></button>
+            </div>
+            <div className="col-sm-12">
+                <img style ={{width: "2500px", height: "750px"}} src = "/image/office2.jpg" className ="img-responsive" alt= "Responsive"/>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
